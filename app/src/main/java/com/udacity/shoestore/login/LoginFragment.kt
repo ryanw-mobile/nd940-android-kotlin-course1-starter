3d6edede371990edf9b57877a0f8f7f8ad9ee6b2
Ryan Wong <ryan@rwmobi.com>
2021-09-07 03:36:43 +0100

    [feature] Login: create LoginFragment, UI and styles

---
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.udacity.shoestore.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
