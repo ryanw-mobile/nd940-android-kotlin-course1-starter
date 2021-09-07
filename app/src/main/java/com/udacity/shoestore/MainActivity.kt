d29a1f580a290e7586a989f1112a036f8cec1995
Ryan Wong <ryan@rwmobi.com>
2021-09-07 04:59:34 +0100

    [feature] MainActivity: implement onSupportNavigateUp()

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,6 +3,7 @@ package com.udacity.shoestore
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -13,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         Timber.plant(Timber.DebugTree())
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
