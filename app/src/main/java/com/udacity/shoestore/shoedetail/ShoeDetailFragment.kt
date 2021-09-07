df4b8b83f36b3f21863453b0e430a6d22a1f7175
Ryan Wong <ryan@rwmobi.com>
2021-09-07 23:47:32 +0100

    [feature] Shoe details: implement basic UI and back navigation

--- app/src/main/java/com/udacity/shoestore/shoedetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoedetail/ShoeDetailFragment.kt
@@ -1,9 +1,13 @@
 package com.udacity.shoestore.shoedetail
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.*
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
 class ShoeDetailFragment : Fragment() {
@@ -21,6 +25,18 @@ class ShoeDetailFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(ShoeDetailViewModel::class.java)
         binding.shoeDetailViewModel = viewModel
 
+        viewModel.eventShouldGoShoeList.observe(viewLifecycleOwner, { shouldGoShoeList ->
+            if (shouldGoShoeList) {
+                onNavigateToShoeList()
+            }
+        })
+        
+        // Additional feature: click on black space to close keyboard
+        binding.shoedetailConstraintlayout.setOnClickListener {
+            val inputMethodManager =
+                requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        }
         setHasOptionsMenu(true)
         return binding.root
     }
@@ -34,5 +50,10 @@ class ShoeDetailFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         menu.clear()
+    }
+
+    private fun onNavigateToShoeList() {
+        findNavController().navigate(R.id.action_shoeDetailFragment_to_shoeListFragment)
+        viewModel.onGoShoeListComplete()
     }
 }
